b608e48f0e7e1051e869e3e25581f59bd9ebe344
Stanislav Zmiev <szmiev2000@gmail.com>
2021-01-20 11:24:03 -0500

    Fix online classes with datetimes not being added

--- unischeduler/text_parser.py
+++ unischeduler/text_parser.py
@@ -1,19 +1,19 @@
 import re
 from typing import List, Tuple, Union
 
+# Good luck figuring this out!
+re_summary = re.compile(r"[A-Z]{3}[A-Z]* \d+[A-Z]? - .+")
+re_details = re.compile(r"(?:[A-Z][a-z]+\n)?(?:[A-Z][a-z])+ \d\d?:\d\d.+\n.+(?:[A-Za-z:\-\s\d,\.])+.+")
+
 
 def parse_schedule(schedule: str) -> List[Union[str, List[str]]]:
-    schedule = "\n".join([l.strip()
-                          for l in schedule.splitlines() if l.strip()])
-    re_summary = re.compile(r"[A-Z]{3}[A-Z]* \d+[A-Z]? - .+")
-    # Good luck figuring this out!
-    re_details = re.compile(
-        r"(?:[A-Z][a-z]+\n)?(?:[A-Z][a-z])+ \d\d?:\d\d.+\n.+(?:[A-Za-z:\-\s\d,\.])+.+")
+    schedule = "\n".join([l.strip() for l in schedule.splitlines() if l.strip()])
     class_summaries = re_summary.findall(schedule)
     classes = re_summary.split(schedule)[1:]  # classes[0] == ''
     sections: List[Union[List[str], str]] = []
     for class_summary, class_info in zip(class_summaries, classes):
-        if "Dropped" in class_info or "Online" in class_info or "Withdrawn" in class_info:
+        # TODO: What if we get an online class without datetimes? Is the regex enough to handle such cases?
+        if "Dropped" in class_info or "Withdrawn" in class_info:
             continue
         current_sections = re_details.findall(class_info)
         for section in current_sections:
@@ -24,16 +24,16 @@ def parse_schedule(schedule: str) -> List[Union[str, List[str]]]:
                 section_type = last_type
             recurrence, location, *professors, dtstart_and_dtend = lines
             professors = [p.replace(",", "") for p in professors]
-            start_time, end_time, weekdays = extract_time_attributes(
-                recurrence)
+            start_time, end_time, weekdays = extract_time_attributes(recurrence)
             dtstart, _, dtend = dtstart_and_dtend.split(" ")
-            sections.append([class_summary, section_type, weekdays,
-                             start_time, end_time, location, professors, dtstart, dtend])
+            sections.append(
+                [class_summary, section_type, weekdays, start_time, end_time, location, professors, dtstart, dtend]
+            )
     return sections
 
 
 # MoWeFr 9:30AM - 10:20AM
 def extract_time_attributes(time_line: str) -> Tuple[str, str, List[str]]:
     weekdays_str, start_time, _, end_time = time_line.split(" ")
-    weekdays = [weekdays_str[i:i + 2] for i in range(0, len(weekdays_str), 2)]
+    weekdays = [weekdays_str[i : i + 2] for i in range(0, len(weekdays_str), 2)]
     return start_time, end_time, weekdays
